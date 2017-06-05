9cd9c1ff90af2acf8087c899dde62870343380e3
lucas <lucas.axm@gmail.com>
2017-06-05 20:02:17 +0000

    t3 - test utilites and examples for t3

--- test.rb
+++ test.rb
@@ -1,3 +1,5 @@
+#!/usr/bin/env ruby
+
 require_relative 'graph'
 require_relative 'graph_theory'
 
@@ -6,9 +8,9 @@ case 2
         graph = Graph.new_from_dot(ARGF.read)
     when 2
         # start = Time.now # debug
-        graph = Graph.gviz2graph(GraphViz.parse( "dots/t3.dot", :path => "." ))
+        graph = Graph.gviz2graph(GraphViz.parse( "dots/emacs24-dep-FIM.dot", :path => "." ))
         # finish = Time.now # debug
         # puts "DEBUG | parse | #{finish-start}" # debug
 end
 gt = GraphTheory.new(graph)
-puts gt.paths_to_sinks.to_s
+puts gt.strongly_connected_components
